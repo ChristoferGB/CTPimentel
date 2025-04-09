98d8f927b0196300f1fea46598aa90a5828f808c
Christofer Galdino Bernardo <christofergaldino2@hotmail.com>
2025-04-09 18:31:06 -0300

    Adds google maps iframe

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,6 +1,11 @@
 import { MapPin } from "lucide-react";
 
 function Location() {
+  const address = "Rua Alberto de Oliveira, 258, Soteco, Vila Velha, ES";
+  const url = `https://www.google.com/maps?q=${encodeURIComponent(
+    address
+  )}&output=embed`;
+
   return (
     <section id="location" className="py-16 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -11,6 +16,15 @@ function Location() {
           </h2>
         </div>
         <div className="bg-white rounded-lg shadow-md p-6">
+          <div className="mb-8" style={{ width: "100%", height: "400px" }}>
+            <iframe
+              title="Mapa"
+              width="100%"
+              height="100%"
+              src={url}
+              allowFullScreen
+            ></iframe>
+          </div>
           <p className="text-lg text-gray-600 text-center">
             Rua Alberto de Oliveira, 258, Soteco
             <br />
