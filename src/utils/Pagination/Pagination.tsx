0b60527f8c269e85b6ea8e83ca9543b9559f5e78
Christofer Galdino Bernardo <christofergaldino2@hotmail.com>
2025-04-29 16:09:54 -0300

    Creates logic on change of selection value

--- src/utils/Pagination/Pagination.tsx
+++ src/utils/Pagination/Pagination.tsx
@@ -4,17 +4,35 @@ import {
   ChevronLeft,
   ChevronRight,
 } from "lucide-react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 function Pagination() {
-  const numberOfLines = [5, 10, 25, "Todos"];
+  const numberOfLinesOptions = [
+    { text: "5", value: 5 },
+    { text: "10", value: 10 },
+    { text: "25", value: 25 },
+    { text: "Todos", value: -1 },
+  ];
+
+  const [numberOfLines, setNumberOfLines] = useState(
+    numberOfLinesOptions[0].value.toString()
+  );
+
+  const handleSelectionChange = (e: string) => {
+    setNumberOfLines(e);
+  };
 
   return (
     <div className="flex flex-row justify-end p-4 gap-12">
       <div>
         <span>Linhas por página: </span>
-        <select className="border border-gray-400">
-          {numberOfLines.map((n) => (
-            <option value={n}>{n}</option>
+        <select
+          className="border border-gray-400"
+          id="selectNumberOfLines"
+          onChange={(e) => handleSelectionChange(e.target.value)}
+        >
+          {numberOfLinesOptions.map((n) => (
+            <option value={n.value}>{n.text}</option>
           ))}
         </select>
       </div>
